f72dd5bcf6584bd0a4393bbfc160180f181ccd8c
pydev <xtfkpi@gmail.com>
2016-03-23 13:52:00 +0200

    it draws

--- 01.py
+++ 01.py
@@ -1,5 +1,5 @@
 from helpers import *
-
+import datetime
 
 class Github_user:
     def __init__(self, login='yglukhov'):
@@ -16,8 +16,39 @@ g = Github_user()
 g.get_user_info_by_rest()
 
 
-class Repo:
+def commit_stat(data):
+    assert len(data) > 0
+    # days=["Monday","Tuesday","Wednesday","Thursday", "Friday", "Saturday","Sunday"]
+    days=["Mon","Tue","Wed","Thu", "Fri", "Sat","Sun"]
+    print(type(data[0]['commit']['committer']['date']))
+    start_date = date_from_string(data[len(data) - 1]['commit']['committer']['date'])
+    end_date = date_from_string(data[0]['commit']['committer']['date'])
+    assert start_date<=end_date
+    date=start_date
+    commits={}
+    while date<=end_date:
+        commits[date]=0
+        # print(days[datetime.date.weekday(date)])
+        date+=datetime.timedelta(days=1)
 
+    for i in range(len(data)):
+        date=date_from_string(data[i]['commit']['committer']['date'])
+        commits[date]+=1
+    # for i in commits.values():
+    #     print(i)
+    # print(commits)
+    points=[]
+    graf_keys=[]
+    while date<=end_date:
+        graf_keys.append(days[datetime.date.weekday(date)])
+        points.append(commits[date])
+        # print(days[datetime.date.weekday(date)], commits[date])
+        date+=datetime.timedelta(days=1)
+
+    return graf_keys,points
+
+
+class Repo:
     def __init__(self, locator='yglukhov/nimx'):
         self.locator = locator
 
@@ -25,22 +56,33 @@ class Repo:
         return True
 
     def get_repo_info(self):
-        url = 'https://api.github.com/repos/'\
+        url = 'https://api.github.com/repos/' \
               + self.locator + '/commits'
         # https://api.github.com/yglukhov/nimx as example
         # curl -i "https://api.github.com/repos/show/nimx"
-        data=dict_from_rest(url)
-        print(data)
-        # for key in data:
-        #     print(key, data[key])
+        data = dict_from_rest(url)
+        # print (date_from_string(data[0]['commit']['committer']['date']))
+        # 2016-03-22T09:38:26Z
 
-r=Repo()
+        # for i in range(len(data)):
+        #     print (date_from_string(data[i]['commit']['committer']['date']))
+        print(commit_stat(data))
+
+
+r = Repo()
 r.get_repo_info()
 
-class Organisation:
-    def __init__(self,locator):
-        self.locator=locator
 
+# string='2016-03-22T09:38:26Z'
+# d=time.strptime(string)
+
+class Weather:
+    pass
+
+
+class Organisation:
+    def __init__(self, locator):
+        self.locator = locator
 
 # 1.1. Для обраного проекту формувати графік:
 # 1) за кількістю нових зірочок за період (тиждень\місяць);
--- helpers.py
+++ helpers.py
@@ -1,8 +1,22 @@
 import json
 import urllib.request as ur
 import urllib.parse as par
+import datetime
+
+
+def date_from_string(s='2016-03-22T09:38:26Z'):
+    d = datetime.date(int(s[0:4]),
+                      int(s[5:7]),
+                      int(s[8:10]))
+    return d
+
+d=date_from_string()
+
+print(d+datetime.timedelta(days=2))
+a={date_from_string():1}
+print(a.keys(),a.values())
 
 def dict_from_rest(url=' https://api.github.com/yglukhov'):
-        html = ur.urlopen(url).read()
-        data = json.loads(html.decode('utf-8'))
-        return data
+    html = ur.urlopen(url).read()
+    data = json.loads(html.decode('utf-8'))
+    return data
