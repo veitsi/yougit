bb5fee1e13b96b2200d56041edd4820e44fb5824
pydev <xtfkpi@gmail.com>
2016-03-25 15:31:57 +0200

    it draws chart from server

--- webapp.py
+++ webapp.py
@@ -2,13 +2,6 @@ from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
-chart = {
-    'labels': ['Tue', 'Wed', 'Thu', 'Fri', 'Sat',
-               'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Mon', 'Tue'],
-    'series': [2, 6, 2, 4, 0, 0, 3, 2, 1, 0, 0, 0, 3, 6, 1]
-}
-
-
 # print(jsonify(chart))
 
 @app.route('/')
@@ -23,7 +16,7 @@ def chart():
     t = {
         'labels': ['Tue', 'Wed', 'Thu', 'Fri', 'Sat',
                    'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Mon', 'Tue'],
-        'series': [2, 6, 2, 4, 0, 0, 3, 2, 1, 0, 0, 0, 3, 6, 1]
+        'series': [[2, 6, 2, 4, 0, 0, 3, 2, 1, 0, 0, 0, 3, 6, 1]]
     }
     return jsonify(t)
 
